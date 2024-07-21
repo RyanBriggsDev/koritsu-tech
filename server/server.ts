991b14d9775e38631d2135440db712364776963a
Ryan Briggs <r@ryanbriggs.dev>
2024-07-21 21:43:27 +0100

    feat: basic backend server wip

--- server/server.ts
+++ server/server.ts
@@ -4,17 +4,13 @@ import cors from "cors";
 const app = express();
 const PORT = 8080;
 
-// Middleware
 app.use(cors());
-app.use(express.json()); // For parsing application/json
+app.use(express.json());
 
-// Routes
 app.get("/login", (req: Request, res: Response) => {
-  // Ideally, you would handle login logic here
   res.json({ token: "test123" });
 });
 
-// Server
 app.listen(PORT, () => {
   console.log(`API is running on http://localhost:${PORT}/login`);
 });
